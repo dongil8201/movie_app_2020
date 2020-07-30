4f8856a5eb0472a7edf8efbfac54a038060a927f
dongil8201 <dongil8201@gmail.com>
2020-07-30 14:03:11 +0900

    Updates

---
+++ precache-manifest.b785b81fee34e8cefa89e8a6af8e75c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54808c57f2fc51593493d93f6eddedc",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "78ce13afe0bca86a41b4",
+    "url": "/movie_app_2020/static/css/main.8c0adce6.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ce13afe0bca86a41b4",
+    "url": "/movie_app_2020/static/js/main.277a2271.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
