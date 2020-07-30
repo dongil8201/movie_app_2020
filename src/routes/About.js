9f795ec323799cc6c5d30441b118232c3ba84f17
dongil8201 <dongil8201@gmail.com>
2020-07-30 11:42:56 +0900

    최종 완성 코드

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+
+function About(props){
+    console.log(props)
+    return (
+        <div className="about__container">
+            <span>about this page............</span>
+            <span>about this page............</span>
+            <span>about this page............</span>
+        </div>
+    )
+}
+
+
+export default About;
